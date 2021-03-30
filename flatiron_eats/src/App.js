1fc17283a32399b6c1f2a433a7fc4ef58df102ec
David Polcari <dpolcari18@gmail.com>
2021-03-30 15:27:37 -0500

    detail view

--- flatiron_eats/src/App.js
+++ flatiron_eats/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 // containers
 import MainContainer from './containers/MainContainer'
@@ -32,8 +32,8 @@ function App() {
     // return <Redirect to={'/user_page'} />
   }
 
-  const validateLogin = (name, email) => {
-    const user = users.find(user => user.name === name && user.email===email)
+  const validateLogin = (password, email) => {
+    const user = users.find(user => user.email === email && user.email===email)
     
     if (user) {
       login(user)
@@ -63,17 +63,18 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <Route exact path ='/' render={_ => user? 
-          <Redirect to='/user_page' /> :
-          <Login validateLogin={validateLogin}
-        />} />
-        <Route path ='/register' render={_ => user?
-          <Redirect to='/user_page' /> :
-          <Register registerUser={registerUser} 
-          />} />
-        <Route path ='/user_page' render={_ => <MainContainer user={user} />} />
-      </div>
+      <Switch>
+          <Route exact path ='/' render={_ => user ? 
+            <Redirect to='/user_page' /> :
+            <Login validateLogin={validateLogin}/>} />
+          <Route path ='/register' render={_ => user ?
+            <Redirect to='/user_page' /> :
+            <Register registerUser={registerUser} />} />
+          <Route path ='/user_page' render={_ => <MainContainer user={user} />} />
+          <Route>
+            <Redirect to='/' />
+          </Route>
+      </Switch>
     </Router>
   );
 }
--- flatiron_eats/src/components/Login.js
+++ flatiron_eats/src/components/Login.js
@@ -6,27 +6,27 @@ import logo from '../images/mod4_logo.png';
 
 function Login({validateLogin}) {
 
-  const [name, setName] = useState('')  
+  const [password, setPassword] = useState('')  
   const [email, setEmail] = useState('')  
 
   const handleSubmit = (e) => {
     e.preventDefault()
     // console.log('hello')
-    validateLogin(name, email)
+    validateLogin(password, email)
   }
 
   return (
     <div>
       <img src={logo} alt="Flatiron Eats Logo" />
       <form onSubmit={(e) => handleSubmit(e)} >
-        <div id="login">
-          <h3>Login Here</h3>
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
-        </div>
         <div>
           <label htmlFor="email">Email</label>
           <input type="email" name="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div id="login">
+          <h3>Login Here</h3>
+          <label htmlFor="password">Password</label>
+          <input type="text" name="password" placeholder="Password" value={password} onChange={(e) => setName(e.target.value)}/>
         </div>
         <input type="submit" value="Login" />
       </form>
