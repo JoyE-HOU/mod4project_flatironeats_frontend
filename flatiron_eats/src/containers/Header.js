b16d0cd6f33f0f17a81cd72f957a55deb153c676
Joy Evans <joyevans@me.com>
2021-03-30 14:48:47 -0500

    header and searchbar updates

--- flatiron_eats/src/containers/Header.js
+++ flatiron_eats/src/containers/Header.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import SearchBar from '../components/SearchBar'
+import Logout from '../components/Logout'
 
 // styling
 import Logo from '../images/mod4_logo_horizontal.png'
 
+const RestuarantURL = 'http://localhost:3000/restaurants'
+
 const Header = () => {
+
+    const [input, setInput] = useState("")
+    const [restaurantList, setRestaurantList] = useState([])
+    // const [restaurantSearch, setRestaurantSearch] = useState("")
+
+    useEffect(async() => {
+        const fetchRestuarants = await fetch(RestuarantURL)
+        const restuarantsRes = await fetchRestuarants.json()
+        setRestaurantList(restuarantsRes)
+    })
+
+    const updateInput = async (input) => {
+        const filtered = restaurantList.filter(restaurant => {
+            return restaurant.location.includes(input)
+            // return restaurant.location.toLowerCase().includes(input.toLowerCase())
+        })
+        setInput(input)
+        setRestaurantList(filtered)
+    }
+
     return(
+        <div>
         <header>
             <img src={Logo} />
         </header>
+            <SearchBar 
+                // input={input}
+                updateInput={updateInput}  
+            />
+            <br></br>
+            <Logout />
+        </div>
     )
 }
 
